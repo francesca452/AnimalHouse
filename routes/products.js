3b01b3d7d5f43fbda6affbb082fca9ef928f5aa3
Luca <luca.orlandello@gmail.com>
2022-10-31 09:15:36 +0100

    backoffice: delete and add new prodocuts

--- routes/products.js
+++ routes/products.js
@@ -25,8 +25,6 @@ router.get('/', async (req, res) => {
 		if (!('pet' in req.query)) throw new Error('Missing pet query parameter');
         const p = await Product.find({ 'pet': req.query.pet }).lean();
 
-		console.log('p\n', p);
-
 		let sections_dict = {};
 		let sections_list = [];
 		for (let i = 0; i < p.length; i++) {
@@ -35,7 +33,6 @@ router.get('/', async (req, res) => {
 			if (!(s in sections_dict)) {
 				sections_dict[s] = sections_list.length;
 				sections_list.push({'section': s, 'products': []});
-				console.log(sections_dict);
 			}
 
 			sections_list[sections_dict[s]].products.push(p[i]);
@@ -49,6 +46,65 @@ router.get('/', async (req, res) => {
         res.status(500).json({message: err.message});
     }    
 })
+
+/* crea un nuovo prodotto, 
+ * NOTA: express cerca il primo route che fa match dall'alto verso il basso del file.
+ * se POST /:id è poszionato prima di POST /new, quando si invia una POST /new new viene preso come ID */
+
+router.post('/new', async (req, res) =>{
+
+    try {
+
+    	const p = new Product(req.body);
+
+        await p.save();
+        res.status(200).end();                   // 201 OK creazione oggetto
+
+    } catch(err) {
+
+        res.status(404).json({message: err.message})        //400 Errore nell'input dell'utente
+    }
+})
+
+//Update one
+router.post('/:id', async (req, res) => {
+
+    try {
+
+		let p = await Product.findById(req.params.id)
+
+		if (('title' in req.body) && (req.body.title !== '')) p.title = req.body.title
+		if (('price' in req.body) && (req.body.price !== '')) p.price = req.body.price
+		if (('pieces_left' in req.body) && (req.body.pieces_left !== '')) p.pieces_left = req.body.pieces_left
+		if (('description' in req.body) && (req.body.description !== '')) p.description = req.body.description
+
+		await p.save()
+
+        res.status(200).end();
+
+    }   
+    catch(err) {
+
+        res.status(400).json({message: err.message})
+    }   
+
+})
+
+//Delete one
+router.delete('/:id', async (req, res) => {
+    try{
+
+        await Product.findByIdAndDelete(req.params.id);
+		res.status(200).end();
+
+    }catch(err) {
+
+        res.status(400).json({message: err.message})
+    }
+})
+
+
+
 
 /*
 //Get all
