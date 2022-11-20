3e0ce040d633255ef9f328068bd1aa3cd3b18a30
Luca <luca.orlandello@gmail.com>
2022-11-20 11:09:38 +0100

    backoffice finito form per modificare dati di un utente

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,16 @@ const express = require('express')
 const router  = express.Router()
 const User    = require('../models/user')
 
+router.post('/:id', async (req, res) => {
+	try {
+		await User.updateOne({ _id: req.params.id }, req.body);
+		res.status(200).end();
+    }   
+    catch(err) {
+        res.status(400).json({message: err.message})
+    }
+});
+
 router.delete('/:id', async (req, res) => {
 	try {
         await User.findByIdAndDelete(req.params.id);
@@ -16,11 +26,15 @@ router.delete('/:id', async (req, res) => {
 router.get('/', async (req, res) => {
 
     try {
-        const userList = await User.find().lean();
+		let dbQuery = {};
+		if ('id' in req.query) dbQuery['_id'] = req.query.id;
+
+        const userList = await User.find(dbQuery).lean();
 		let userListNoPsw = [];
 		for (let i = 0; i < userList.length; i++) {
 			userListNoPsw.push(
 				{
+					'_id': userList[i]._id,
 					'username': userList[i].username,
 					'email': userList[i].email
 				});
